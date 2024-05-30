e36b0288110f5aebb75bf0b914fa6acdd7da4d8b
Win2000_dev_community <154258820+Win2000DevCommunity@users.noreply.github.com>
2024-05-30 01:59:47 +0100

    Add files via upload

---
+++ NT4/Code/main.cpp
@@ -0,0 +1,15 @@
+#include <windows.h>
+#include "MainWindow.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MainWindow::RegisterAndCreateWindow(hInstance, nCmdShow);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
