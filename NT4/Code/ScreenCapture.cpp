0faaa863c23c8690ed3d197eab9e1e625fe79667
Win2000_dev_community <154258820+Win2000DevCommunity@users.noreply.github.com>
2024-05-30 00:37:46 +0000

    Update ScreenCapture.cpp
    add the Select window functionality

--- NT4/Code/ScreenCapture.cpp
+++ NT4/Code/ScreenCapture.cpp
@@ -33,3 +33,27 @@ HBITMAP ScreenCapture::CaptureRegion(RECT* rect, int &width, int &height) {
 
     return hBitmap;
 }
+HBITMAP ScreenCapture::CaptureSelectedWindow(int &width, int &height) {
+    HWND hwndSelected = GetForegroundWindow();  // Get the handle of the selected window
+    if (!hwndSelected) {
+        return NULL;
+    }
+
+    HDC hWindowDC = GetWindowDC(hwndSelected);
+    HDC hMemoryDC = CreateCompatibleDC(hWindowDC);
+
+    RECT windowRect;
+    GetWindowRect(hwndSelected, &windowRect);
+
+    width = windowRect.right - windowRect.left;
+    height = windowRect.bottom - windowRect.top;
+    HBITMAP hBitmap = CreateCompatibleBitmap(hWindowDC, width, height);
+
+    SelectObject(hMemoryDC, hBitmap);
+    BitBlt(hMemoryDC, 0, 0, width, height, hWindowDC, 0, 0, SRCCOPY);
+
+    DeleteDC(hMemoryDC);
+    ReleaseDC(hwndSelected, hWindowDC);
+
+    return hBitmap;
+}
