aa38065515f3aa26a938fcb4c41a946e7af4acbd
Win2000_dev_community <154258820+Win2000DevCommunity@users.noreply.github.com>
2024-05-30 00:54:48 +0000

    Update main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,13 +3,10 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     MainWindow::RegisterAndCreateWindow(hInstance, nCmdShow);
-
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return (int)msg.wParam;
 }
-
