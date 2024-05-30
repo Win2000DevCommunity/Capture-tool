28ca08dfa40622fc3538253da9181843e68023b3
Win2000_dev_community <154258820+Win2000DevCommunity@users.noreply.github.com>
2024-05-30 00:53:51 +0000

    Update ScreenCapture.h

--- Code/ScreenCapture.h
+++ Code/ScreenCapture.h
@@ -7,6 +7,7 @@ class ScreenCapture {
 public:
     static HBITMAP CaptureScreen(int &width, int &height);
     static HBITMAP CaptureRegion(RECT* rect, int &width, int &height);
+    static HBITMAP CaptureSelectedWindow(int &width, int &height); // Declare the new function
 };
 
 #endif // SCREENCAPTURE_H
