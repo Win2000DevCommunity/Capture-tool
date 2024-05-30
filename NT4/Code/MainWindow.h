0048ff33b2e197290d4a4d58b43c35f8791b64df
Win2000_dev_community <154258820+Win2000DevCommunity@users.noreply.github.com>
2024-05-30 02:23:21 +0100

    Add files via upload

---
+++ NT4/Code/MainWindow.h
@@ -0,0 +1,12 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <windows.h>
+
+class MainWindow {
+public:
+    static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+    static void RegisterAndCreateWindow(HINSTANCE hInstance, int nCmdShow);
+};
+
+#endif // MAINWINDOW_H
