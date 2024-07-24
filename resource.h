3525c78ecbfa3e13c473905661b40f09011784e2
Win2000_dev_community <154258820+Win2000DevCommunity@users.noreply.github.com>
2024-07-24 04:09:36 -0500

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,23 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#define IDS_APP_TITLE           101
+#define IDS_SELECT_MODE         102
+#define IDS_CAPTURE             103
+#define IDS_SAVE                104
+#define IDS_OPEN                105
+#define IDS_FULL_SCREEN         106
+#define IDS_SELECT_REGION       107
+#define IDS_SELECTED_WINDOW     108
+#define IDS_CAPTURE_ERROR       109
+#define IDS_SAVE_ERROR          110
+#define IDS_OPEN_ERROR          111
+#define IDI_ICON1               201
+#define IDI_ICON_CAPTURE        202
+#define IDI_ICON_SAVE           203
+#define IDI_ICON_OPEN           204
+
+
+#endif // RESOURCE_H
+
+
